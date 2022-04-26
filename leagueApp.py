9ea5309a172de9b3fe218074b6c76b0ce50547fc
Dylan Shade <dpshadey22@gmail.com>
2022-04-26 16:28:36 -0400

    adds vision score and kda

--- leagueApp.py
+++ leagueApp.py
@@ -33,9 +33,11 @@ for index, row in df.iterrows():
 
 
 st.title("League App")
-f"""
-### League Stats for 41619 and Yongbin
-"""
+
+# f"""
+# ### League Stats for 41619 and Yongbin
+# """
+
 col1, col2 = st.columns(2)
 with col1:
     myChamp = st.text_input("My Champion")
@@ -53,21 +55,49 @@ if myChamp != "" and partnerChamp == "":
 elif myChamp == "" and partnerChamp != "":
     df = df[df.PartnerChampionID == partnerChamp]
 elif myChamp != "" and partnerChamp != "":
-    df = df[(df.ChampionID == myChamp) & (df.PartnerChampionID == partnerChamp)]
+    df = df[(df.ChampionID == myChamp) & (
+        df.PartnerChampionID == partnerChamp)]
 
 
 w = 0
+myKDA = 0
+themKDA = 0
+
+myVS = 0
+themVS = 0
+
 for index, row in df.iterrows():
     if row["Win/Lost"]:
         w += 1
 
+    myKDA += row["KDA"]
+    themKDA += row["PartnerKDA"]
+
+    myVS += row["VisionScore"]
+    themVS += row["PartnerVisionScore"]
+
+
+myKDA, themKDA = round(myKDA / df.shape[0], 2), round(themKDA / df.shape[0], 2)
+myVS, themVS = round(myVS / df.shape[0], 2), round(themVS / df.shape[0], 2)
+
+
 wr = round(w / df.shape[0], 4) * 100
 
-newCol1, newCol2, newCol3, newCol4 = st.columns(4)
+
+newCol1, newCol2, newCol3, newCol4, newCol5 = st.columns(5)
 with newCol1:
     st.metric("Winrate", f"{wr}%", delta=None, delta_color="normal")
 
-with newCol4:
+with newCol2:
+    st.metric("My KDA", f"{myKDA}", delta=None, delta_color="normal")
+    st.metric("Their KDA", f"{themKDA}", delta=None, delta_color="normal")
+
+with newCol3:
+    st.metric("My VS", f"{myVS}", delta=None, delta_color="normal")
+    st.metric("Their VS", f"{themVS}", delta=None, delta_color="normal")
+
+
+with newCol5:
     st.metric("41619 Top Champ", f"{max(meDict, key = meDict.get)}")
     st.metric(
         "Yongbin Top Champ",
